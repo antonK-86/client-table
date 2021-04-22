9d46687daca6127a754ff6ffc75a833b6598ee1f
antonK-86 <you@example.com>
2021-04-22 22:47:21 +0300

    preview

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { data } from "./dblocal/db";
 import TableContainer from "./components/table/TableContainer";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
 function App() {
-  return (
-      <TableContainer/>
-  );
+  const mdata = data;
+  return <TableContainer data={mdata} />;
 }
 
 export default App;
